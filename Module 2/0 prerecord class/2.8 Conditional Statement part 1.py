da7a04b2d94971a613549487700bf35792d94da4
shakilahamedriaz <shakilahamedriaz@gmail.com>
2025-02-03 22:39:44 +0600

    file updated

---
+++ Module 2/0 prerecord class/2.8 Conditional Statement part 1.py
@@ -0,0 +1,8 @@
+#value "rain" is equal to 1 or not
+rain = int(input())
+
+#indentation : if - else blook with 4 count space
+if rain == 10:
+    print("Equal")
+else:
+    print("Not Equal")
---
+++ Module 2/0 prerecord class/2.9 Conditiona Statement Part 2.py
@@ -0,0 +1,9 @@
+#chek a number is positive, negtative or zero
+number = int(input("Enter the number : "))
+
+if number > 0:
+    print("Positive Number")
+elif number < 0:
+    print("Negative Number")
+else:
+    print("Number is Zero")
