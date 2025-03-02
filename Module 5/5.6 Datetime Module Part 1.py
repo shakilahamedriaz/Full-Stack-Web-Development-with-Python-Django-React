01df1dbbe59e3cddf26da3b599f5ed3d0331abba
shakilahamedriaz <shakilahamedriaz@gmail.com>
2025-03-02 23:30:10 +0600

    py datetime module updated

---
+++ Module 5/5.6 Datetime Module Part 1.py
@@ -0,0 +1,52 @@
+#Datetime module
+import datetime
+
+#date and time now
+current_time = datetime.datetime.now()
+print(current_time)
+#2025-03-02 17:05:04.117743
+
+
+#only date today
+today_date = datetime.date.today()
+print(today_date)
+#2025-03-02
+
+
+#today_time
+today_time = datetime.datetime.now().time()
+print(today_time)
+#17:17:30.753480
+
+
+#custom_datetime                    year month date 10 30 min
+custome_datetime = datetime.datetime(2030, 2, 20, 10, 30, 0)
+print(custome_datetime)
+#2030-02-20 10:30:00
+
+custome_datetime2 = datetime.datetime(2030, 2, 20, 10)
+print(custome_datetime2)
+#2030-02-20 10:00:00
+
+
+formatted_date = current_time.strftime("%Y/%m/%d %H:%M:%S")  
+print(formatted_date)
+#2025/03/02 17:44:09
+
+formatted_date2 = current_time.strftime("%y/%m/%d %H:%M:%S")
+print(formatted_date2)
+#25/03/02 17:45:37
+
+formatted_date3 = current_time.strftime("%y/%B/%d %H:%M:%S")
+print(formatted_date3)
+#25/March/02 17:46:19
+
+
+formatted_date4 = current_time.strftime("%y/%b/%d %H:%M:%S")
+print(formatted_date4)
+#25/Mar/02 17:46:19
+
+
+formatted_date5 = current_time.strftime("%y/%b/%a %H:%M:%S")
+print(formatted_date5)
+#25/Mar/Sun 17:47:34
