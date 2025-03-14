03d9a919d39f5a3047068ddf2c654adb20340971
shakilahamedriaz <shakilahamedriaz@gmail.com>
2025-03-15 02:02:56 +0600

    advance sql  noted

---
+++ Module 8 SQL Advance/0 Prerecord Class/1 advancePractise.sql
@@ -0,0 +1,33 @@
+-- Active: 1741979921591@@127.0.0.1@3306@ecommerce_db
+
+
+--Rename column
+ALTER TABLE students  
+CHANGE COLUMN course_id id INT;
+-- Yes, ALTER is used to modify an existing table, such as renaming columns, changing data types, adding or dropping columns, etc.
+
+
+--Rename Table
+ALTER TABLE university 
+RENAME TO uni
+--another way to rename table
+RENAME TABLE uni TO Diu
+;   --       old table        new table
+
+
+
+--Drop column
+ALTER TABLE students
+DROP COLUMN active
+;
+
+
+--Drop Talbe
+DROP Table univeristy
+;
+
+
+--Drop Database
+DROP DATABASE database_name
+;
+
