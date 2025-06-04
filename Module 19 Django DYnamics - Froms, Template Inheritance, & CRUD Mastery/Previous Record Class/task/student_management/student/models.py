10a39a344ac3cfa95a3cf916bbeea7c805f76797
shakilahamedriaz <shakilahamedriaz@gmail.com>
2025-06-05 03:59:25 +0600

    add Connect with Database using from
    from data to database

--- Module 19 Django DYnamics - Froms, Template Inheritance, & CRUD Mastery/Previous Record Class/task/student_management/student/models.py
+++ Module 19 Django DYnamics - Froms, Template Inheritance, & CRUD Mastery/Previous Record Class/task/student_management/student/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    password = models.CharField(max_length=100) 
+    checkbox = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+    
+    
--- Module 19 Django DYnamics - Froms, Template Inheritance, & CRUD Mastery/Previous Record Class/task/student_management/student/views.py
+++ Module 19 Django DYnamics - Froms, Template Inheritance, & CRUD Mastery/Previous Record Class/task/student_management/student/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , HttpResponse
+from .import models
 # Create your views here.
 
 # There are three types of froms in Django
@@ -10,4 +10,28 @@ from django.shortcuts import render
 
 def home(request):
     print(request.POST)
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        password = request.POST.get('password')
+        checkbox = request.POST.get('checkbox')
+
+        if checkbox == 'on':
+            checkbox = True
+        else:
+            checkbox = False
+
+        student = models.Student(
+            name=name,
+            email=email,
+            phone=phone,
+            password=password,
+            checkbox=checkbox
+        ) # student classer ekta object create korlam
+        student.save() #student table ekt record make korlam
+        #return render(request, 'student/student.html')
+        return HttpResponse('Data saved successfully')
+    
+        
     return render(request, 'student/index.html')
