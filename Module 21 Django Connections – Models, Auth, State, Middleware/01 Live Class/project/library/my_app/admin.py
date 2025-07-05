536c64d3e22fb9d2cc8f8bd6941fa32689404d3f
shakilahamedriaz <shakilahamedriaz@gmail.com>
2025-07-05 23:29:50 +0600

    updated

--- Module 21 Django Connections – Models, Auth, State, Middleware/01 Live Class/project/library/my_app/admin.py
+++ Module 21 Django Connections – Models, Auth, State, Middleware/01 Live Class/project/library/my_app/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Author, Book, Profile, Course, Student
 
-# Register your models here.
 admin.site.register(Author)
 admin.site.register(Book)
 admin.site.register(Profile)
