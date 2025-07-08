fe031a2d464ded0bd042b97e1f5c87f54c2ceae1
shakilahamedriaz <shakilahamedriaz@gmail.com>
2025-07-09 01:58:07 +0600

    Tech Story updated

---
+++ Module 23 Building a Full Functional Blog Project/Prerecord Class/myblog/blog/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from ckeditor.fields import RichTextField
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = RichTextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    tag = models.ManyToManyField(Tag)
+    view_count = models.PositiveBigIntegerField(default=0)
+    liked_users = models.ManyToManyField(User, related_name='liked_posts')
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
