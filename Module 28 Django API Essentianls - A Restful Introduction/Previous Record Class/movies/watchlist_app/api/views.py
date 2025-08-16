96bf69e062478a09c74a2cb2db388dcc444e6fae
shakilahamedriaz <shakilahamedriaz@gmail.com>
2025-08-16 18:50:53 +0600

    updated

--- Module 28 Django API Essentianls - A Restful Introduction/Previous Record Class/movies/watchlist_app/api/views.py
+++ Module 28 Django API Essentianls - A Restful Introduction/Previous Record Class/movies/watchlist_app/api/views.py
@@ -2,12 +2,66 @@ from watchlist_app import models
 from .import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 
 
+# @api_view()
+# def movie_list(request):
+#     movies = models.MovieList.objects.all()  #python object
+#     serializer = serializers.MovieListSerializer(movies, many=True) #python object ke jeson e covert korbe
+#     return Response(serializer.data)
 
-@api_view()
+
+
+@api_view(['GET', 'POST'])
 def movie_list(request):
-    movies = models.MovieList.objects.all()  #python object
-    serializer = serializers.MovieListSerializer(movies, many=True) #python object ke jeson e covert korbe
-    return Response(serializer.data)
+    if request.method == 'GET':
+        movies = models.MovieList.objects.all()  #python object
+        serializer = serializers.MovieListSerializer(movies, many=True) #python object ke jeson e covert korbe
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = serializers.MovieListSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_NOT_FOUND)
+
+
+#GET --> Read (Retrieve a list of movies)
+#POST --> Create (Add a new movie)
+#PUT --> Update (whole object ke pathano hoy)
+#DELETE --> Delete (Remove a movie)
+
+#PATCH --> Update Specific part (partial object ke pathano hoy)
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def movie_detail(request, pk):
+    movie = get_object_or_404(models.MovieList, pk=pk)
+
+    if request.method == 'GET':
+        serializer = serializers.MovieListSerializer(movie)
+        return Response(serializer .data, status=status.HTTP_200_OK)
+
+    elif request.method == 'PUT':
+        serializer = serializers.MovieListSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'PATCH':
+        serializer = serializers.MovieListSerializer(movie, data=request.data, partial=True) # partial update
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        movie.delete()
+        return Response({'message': 'Movie deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+
+
